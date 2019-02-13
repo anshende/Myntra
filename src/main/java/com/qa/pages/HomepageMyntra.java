ca75ca64fc82eab7a54d05623c663f88c658a829
Aniket Shende <aniketshende1309@gmail.com>
2019-02-13 23:02:23 +0530

    This is second commit for Myntra site

--- src/main/java/com/qa/pages/HomepageMyntra.java
+++ src/main/java/com/qa/pages/HomepageMyntra.java
@@ -3,18 +3,38 @@ package com.qa.pages;
 import java.io.FileNotFoundException;
 
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.basepackage.TestBase;
 
 public class HomepageMyntra extends TestBase {
 	
+	WebDriverWait wait;
 	
 	@FindBy(xpath="//div[@class='desktop-navLinks']/div[1]")
 	WebElement men;
 	
+	@FindBy(xpath="//ul[@class='desktop-navBlock']/li[2]/a[@href='https://www.myntra.com/men-tshirts']")
+	WebElement men_tshirt;
+	
+	
+	
 	public HomepageMyntra() throws FileNotFoundException {
 		super();
+		PageFactory.initElements(driver, this);
 	}
-
+	
+	public TshirtpageMyntra men_cat() {
+		Actions action=new Actions(driver);
+		action.moveToElement(men).perform();
+		wait.until(ExpectedConditions.elementToBeClickable(men_tshirt));
+		
+		return new TshirtpageMyntra();
+	}
+   
+	
 }
--- src/test/java/com/testpackage/HomepageMyntraTest.java
+++ src/test/java/com/testpackage/HomepageMyntraTest.java
@@ -1,5 +1,32 @@
 package com.testpackage;
 
-public class HomepageMyntraTest {
+import java.io.FileNotFoundException;
+
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import com.basepackage.TestBase;
+import com.qa.pages.HomepageMyntra;
+import com.qa.pages.TshirtpageMyntra;
+
+public class HomepageMyntraTest extends TestBase {
+	
+	HomepageMyntra homepage;
+	TshirtpageMyntra tshirt;
+	
+	public HomepageMyntraTest() throws FileNotFoundException {
+	    super();
+	}
+	
+	@BeforeClass
+	public void setup() throws FileNotFoundException {
+		initialization();
+		homepage=new HomepageMyntra();
+	}
+	
+	@Test
+	public void mentshirt() {
+		tshirt=homepage.men_cat();
+	}
 
 }
